a84ba1f58e34ec978b48faf49db171b0089b1142
Ash-sc <mail@ashshen.cc>
2020-05-14 17:18:42 +0800

    change .gitignore

--- config/user.config.js
+++ config/user.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  telegramToken: '',
-  telegramChatId: '',
-  httpsProxy: '',
+  telegramToken: '1162583899:AAEyL_Vz1ExCDlwo51eX6MBzyjjM7_L9lu0',
+  telegramChatId: '1237007808',
+  httpsProxy: 'http://127.0.0.1:1087',
 };
